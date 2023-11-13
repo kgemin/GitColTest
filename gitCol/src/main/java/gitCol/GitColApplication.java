cc5a0bc66d83b42da1efdbd248548995cebadc21
HKEDU <HKEDU@DESKTOP-0NM39QU>
2023-11-13 16:56:33 +0900

    GitColTest from kgemin

---
+++ gitCol/src/main/java/gitCol/GitColApplication.java
@@ -0,0 +1,13 @@
+package gitCol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitColApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitColApplication.class, args);
+	}
+
+}
---
+++ gitCol/src/test/java/gitCol/GitColApplicationTests.java
@@ -0,0 +1,13 @@
+package gitCol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitColApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
